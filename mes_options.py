b6c943967cc4027846b832b43a6e4fd80827ba87
Semaine52 <semaine52@gmail.com>
2019-12-13 18:01:10 +0100

    Semaine52Lab /  AuFilDuBoamp / EdTech / MarchésPublics / 2019-2020

--- mes_options.py
+++ mes_options.py
@@ -1,46 +1,13 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#IPYTHON  INTERACTIVESHELL\n",
-    "from IPython.core.interactiveshell import InteractiveShell\n",
-    "InteractiveShell.ast_node_interactivity = \"all\" \n",
-    "\n",
-    "#IPYTHON DISPLAY\n",
-    "from IPython.display import HTML\n",
-    "    \n",
-    "# PANDAS\n",
-    "import pandas as pd\n",
-    "pd.set_option('display.notebook_repr_html', True)\n",
-    "pd.set_option('display.max_colwidth', -1)\n",
-    "pd.set_option('display.max_columns', None)\n",
-    "pd.set_option('display.max_rows', 2000)"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+#IPYTHON  INTERACTIVESHELL
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all" 
+
+#IPYTHON DISPLAY
+from IPython.display import HTML
+    
+# PANDAS
+import pandas as pd
+pd.set_option('display.notebook_repr_html', True)
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 5000)
